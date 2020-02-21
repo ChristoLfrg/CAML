7df346ab3a4e7af64f9ac632d6965bbb4bd57a0d
ChristoLfrg <55689597+ChristoLfrg@users.noreply.github.com>
2020-02-21 10:55:07 +0100

    Add files via upload

--- Projet.ml
+++ Projet.ml
@@ -35,9 +35,9 @@ liste_succ 7 graphe1;;
 (** 	Pour inverser un graphe, (a,li) chaque membre de li doit recevoir comme successeur le sommet a 
 	J'ai donc besoin d'une fonction qui fasse cela sur chaque sommet (couple) du graphe
 	
-	J'ai [(1,[6;7;8])]
+	J'ai [(1,[6;7;8]),(6,[5;7])]
 	       s   li
-	Je veux [(6,[1]);(7,[1]);(8,[1])]
+	Je veux [(5,[6]);(6,[1]);(7,[1;6]);(8,[1])]
 	             s       s       s
 **)
 	(* faire succéder un sommet «s» à chacun des sommets d'une liste «li» *)
@@ -48,7 +48,7 @@ let rec inserer_liste_couple s li =
 	
 (**Ex : (1,[3;6]) -> [(3,[1]);(6,[1])]**)
 inserer_liste_couple 1 [3;6];;
- 
+
 let rec inverse_graphe g =
 	match g with
 	| [] -> []
@@ -57,16 +57,15 @@ let rec inverse_graphe g =
 let rec distribuer s li =
 	List.fold_left (fun f l -> match li with x::r ->  ) [] li;; 
 
+(** D'ABORD créer un graphe avec la liste des sommets G2 = (1;2;3;4;5;6;7;8)
+	G1 = [(1,[6;7;8]),(6,[5;7])]
+	PUIS pour chaque sommets successeurs, donner au sommet correspondant le sommet de base  *)
+(*
 let inverse_graphe2 g =
-	List.fold_left (inserer_liste_couple) [] g;;
+	List.fold_left (inserer_liste_couple) [] g;;*)
 
 inverse_graphe graphe1;;
 
-
-
-
-
-construction_graphe1 s li = (s,li);;
 
 
 
